b2dec4b6c2024dad6c55c5c05906c0c8796e9025
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-04-01 14:24:26 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,5 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
-config.ssl_options = { hsts: { subdomains: false } }
+
 # Initialize the Rails application.
 Rails.application.initialize!
