e588a53e18d0dca7e32aa8134442699cea9e40ee
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-04-01 14:24:35 +0000

    Automatically backed up by Learn

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ Bundler.require(*Rails.groups)
 
 module OmniauthLab
   class Application < Rails::Application
+    
+    config.ssl_options = { hsts: { subdomains: false } }
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
